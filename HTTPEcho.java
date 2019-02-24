ec0136c0fd8fdf0ef125ef19b2a9ee30a47d750d
Frida Mattsson <34581085+mattssonf@users.noreply.github.com>
2019-02-24 14:15:15 +0100

    Add files via upload

---
+++ HTTPEcho.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class HTTPEcho {
+    public static void main( String[] args) throws IOException {
+
+      int portnr = Integer.parseInt(args[0]);
+      ServerSocket welcomeSocket = new ServerSocket(portnr);
+
+      while(true){
+
+        Socket connectionSocket = welcomeSocket.accept();
+
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        OutputStream outToClient = connectionSocket.getOutputStream();
+
+        String line = inFromClient.readLine();
+        while(!line.isEmpty()){
+
+          line = line + "\r\n";
+          outToClient.write(line.getBytes("UTF-8"));
+          line = inFromClient.readLine();
+
+        }
+        connectionSocket.close();
+      }
+    }
+}
